6447c347d1fb5d149c60548fb606fb4d8d7dedd1
darmsDD <ivandinizdobbin2@gmail.com>
2019-09-15 14:20:15 -0300

    Improving interface

--- src/verdadeira_interface.py
+++ src/verdadeira_interface.py
@@ -29,11 +29,36 @@ Green = (0,128,0)
 Purple = (128,0,128)
 Teal = (0,128,128)
 Navy = (0,0,128)
-COLORS = list((black,white,red,lime,blue,Yellow,Cyan,Magenta,Silver,Gray,Maroon,Olive,Green,Purple,Teal))
+
+COLORS = list((black,white,red,lime,blue,Cyan,Silver,Gray,Olive,Green,Purple,Teal,(128,0,0),
+(139,0,0),(165,42,42),(178,34,34),(220,20,60),(255,99,71),(255,127,80),(205,92,92),(240,128,128),(233,150,122),
+(250,128,114),(255,160,122),(255,69,0),(255,140,0),(255,165,0),(255,215,0),(184,134,11),(218,165,32),(238,232,170),
+(189,183,107),(240,230,140),(154,205,50),(85,107,47),(144,238,144),(143,188,143),(46,139,87),(102,205,170),(32,178,170),
+(47, 79, 79),(0,139,139),(25,25,112),(0,0,128),(138,43,226),(75,0,130),(106,90,205),(139,0,139),(186,85,211),(238,130,238),
+(255,0,255),(199,21,133),(219,112,147),	(255,20,147),(255,105,180),(245,222,179)))
 
 def text_objects(text, font,color):
     textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
+
+
+def paint_button(largeText,color,inactiveColor):
+    x_difficulty = 0
+    mouse = pygame.mouse.get_pos()
+    for k in range(9):
+        if 50 + x_difficulty + 90 > mouse[0] > 50 + x_difficulty and 250 + 50 > mouse[1] > 250:
+            pygame.draw.rect(screen, inactiveColor, (50 + x_difficulty, 250, 90, 50))
+            txt_surf, txt_rect = text_objects(difficulty[k], largeText, blue)
+            txt_rect.center = ((50 + x_difficulty + (90 / 2)), (250 + (50 / 2)))
+            screen.blit(txt_surf, txt_rect)
+        else:
+            pygame.draw.rect(screen, color, (50 + x_difficulty, 250, 90, 50))
+            txt_surf, txt_rect = text_objects(difficulty[k], largeText, blue)
+            txt_rect.center = ((50 + x_difficulty + (90 / 2)), (250 + (50 / 2)))
+            screen.blit(txt_surf, txt_rect)
+
+        x_difficulty += 100
+
 
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf',115)
@@ -47,6 +72,10 @@ def message_display(text):
 
     game_loop()
 
+
+
+
+
 def game_intro():
 
     intro = True
@@ -56,13 +85,14 @@ def game_intro():
     # This surface is used to adjust the alpha of the txt_surf.
     alpha_surf = pygame.Surface(txt_surf.get_size(), pygame.SRCALPHA)
     alpha = 255  # The current alpha value of the surface.
+
     while intro:
         for event in pygame.event.get():
             #print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYUP:
                     choose_difficulty()
         alpha -= 4
         if alpha > 0:
@@ -74,6 +104,7 @@ def game_intro():
             # alpha_surf onto it with the BLEND_RGBA_MULT flag.
             txt_surf.blit(alpha_surf, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
         else:
+            #to make the word appear again so it can fade
             font = pygame.font.Font(None, 30)
             orig_surf = font.render('Press any key to continue', True, black)
             txt_surf = orig_surf.copy()
@@ -82,11 +113,14 @@ def game_intro():
             alpha = 255  # The current alpha value of the surface.
 
         screen.fill(white)
+
         largeText = pygame.font.Font('freesansbold.ttf',80)
         TextSurf, TextRect = text_objects("Choose your fate", largeText,black)
         TextRect.center = ((width/2),(height/3))
+
         txt_rect = txt_surf.get_rect()
         txt_rect.center  = ((width/2),(height*0.8))
+
         screen.blit(TextSurf, TextRect)
         screen.blit(txt_surf,txt_rect)
         #screen.blit(txt_surf, (500, 400))
@@ -102,24 +136,22 @@ def choose_difficulty():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse = pygame.mouse.get_pos()
+                x_difficulty = 0
+                for k in range(9):
+                    if 50 + x_difficulty + 90 > mouse[0] > 50 + x_difficulty and 250 + 50 > mouse[1] > 250:
+                        break
+                    x_difficulty += 100
+                game_loop(x_difficulty)
+
         screen.fill(white)
-        x_difficulty=0
+
         largeText = pygame.font.Font('freesansbold.ttf', 16)
-        for k in range (9):
-            pygame.draw.rect(screen,black,(50+x_difficulty, 250, 90, 50))
-            txt_surf, txt_rect = text_objects(difficulty[k], largeText, blue)
-            txt_rect.center = ((50 + x_difficulty + (90 / 2)), (250 + (50 / 2)))
-            screen.blit(txt_surf, txt_rect)
-            x_difficulty += 100
-        mouse = pygame.mouse.get_pos()
-        x_difficulty=0
-        for k in range (9):
-            if 50+x_difficulty+90> mouse[0] > 50+x_difficulty and 250+50>mouse[1]>250:
-                pygame.draw.rect(screen, red ,(50+x_difficulty, 250, 90, 50))
-            txt_surf, txt_rect = text_objects(difficulty[k], largeText, blue)
-            txt_rect.center = ((50 + x_difficulty + (90 / 2)), (250 + (50 / 2)))
-            screen.blit(txt_surf, txt_rect)
-            x_difficulty += 100
+        paint_button(largeText,black,red)
+
+
+
 
 
         largeText = pygame.font.Font('freesansbold.ttf', 80)
@@ -130,37 +162,66 @@ def choose_difficulty():
         clock.tick(30)
 
 
-def game_loop():
-    running = True
-    #for a in COLORS:
-    #   print(a)
-    x = 10
-    y = 10
-    back=0
-    front=1
-    while running:
-        random.shuffle(COLORS)
+def draw1(level,width_card,temp):
+    x=0
+    for i in range(level):
+        if i == (level - 1):
+            width_card_2 = width - x
+            pygame.draw.rect(screen, COLORS[i], (x, 0, width_card_2, height))
+        else:
+            pygame.draw.rect(screen, COLORS[i], (x, 0, width_card, height))
+        x += width_card
+        temp.append(COLORS[i])
+
+def draw2(level,width_card,temp):
+    x = 0
+    mouse = pygame.mouse.get_pos()
+    for i in range(level):
+        if i == (level - 1):
+            width_card_2 = width - x
+            pygame.draw.rect(screen, temp[i], (x, 0, width_card_2, height))
+            if x + width_card > mouse[0] > x and height > mouse[1] > 0:
+                pygame.draw.rect(screen, black, (x, 0, width_card_2, height), 5)
+        else:
+
+            pygame.draw.rect(screen, temp[i], (x, 0, width_card, height))
+            if x + width_card > mouse[0] > x and height > mouse[1] > 0:
+                pygame.draw.rect(screen, black, (x, 0, width_card, height), 5)
+
+        x += width_card
+def game_loop(level):
+    level //= 100
+    level += 1
+    level *= 5
+    width_card = width // level
+    random.shuffle(COLORS)
+    temp = []
+    v=1
+    while True:
         for event in pygame.event.get():
+            #print(event)
             if event.type == pygame.QUIT:
-                running = False
                 pygame.quit()
                 quit()
-        screen.fill(background_colour)
-        pygame.draw.rect(screen,(255,0,0),(890,250,20,100))
-        if x == 700 or back==1:
-            pygame.draw.rect(screen,COLORS[random.randint(0,len(COLORS)-1)], (190+x,250,20,100))
-            #print("b")
-            x-=10
-            back=1
-            front=0
-        if x==0 or front ==1:
-            pygame.draw.rect(screen,COLORS[random.randint(0,len(COLORS)-1)],(190+x,250,20,100))
-            #print("a")
-            x+=10
-            front=1
-            back=0
+            #if event.type == pygame.MOUSEBUTTONUP:
+                #correct_choice()
+            if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_ESCAPE:
+                        choose_difficulty()
+
+        screen.fill(white)
+        if v==1:
+            draw1(level,width_card,temp)
+            random.shuffle(temp)
+            v=0
+            pygame.display.update()
+            time.sleep(5)
+        else:
+            draw2(level,width_card,temp)
+
         pygame.display.update()
-        clock.tick(120)
+
+
 
 game_intro()
 game_loop()
